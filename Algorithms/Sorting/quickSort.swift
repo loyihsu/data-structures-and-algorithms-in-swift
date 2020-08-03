a820213822af92b933e2cf4b5556627c412562fc
loyihsu <loyi.hsu@gmail.com>
2020-08-03 19:56:58 +0800

    Update quickSort implementation.

--- Algorithms/Sorting/quickSort.swift
+++ Algorithms/Sorting/quickSort.swift
@@ -9,24 +9,20 @@ import Foundation
 
 extension Array where Element == Int {
     mutating func quickSort(left: Int, right: Int) {
-        var i = left
-        var j = right
+        guard left >= 0 && right < self.count && left < right else { return }
+        var i = left, j = right
         let standard = self[left]
-
-        if left > right { return }
-
+        
         while i != j {
             while self[j] >= standard && i < j { j -= 1 }
             while self[i] <= standard && i < j { i += 1 }
             if i < j { swapAt(i, j) }
         }
-
+        
         self[left] = self[i]
         self[i] = standard
-
-        print(self)
-
-        if i - 1 > 0 { quickSort(left: left, right: i-1) }
-        if i + 1 < self.count { quickSort(left: i+1, right: right) }
+        
+        quickSort(left: left, right: i-1)
+        quickSort(left: i+1, right: right)
     }
 }
